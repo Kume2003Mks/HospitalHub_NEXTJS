883b37bbd5441d8e8ad4fa7156989405f7ef2aaf
Kume2003Mks <105347897+Kume2003Mks@users.noreply.github.com>
2024-09-06 00:03:30 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
         remotePatterns: [
             {
                 protocol: 'https',
